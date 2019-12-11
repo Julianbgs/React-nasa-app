ab7a0c5ea35b2e5db4f849aeaa6cb6f7db1e85c0
Julian Blagis <julianbgs96@gmail.com>
2019-12-11 23:45:10 +0200

    add component Search, create logic for search photos

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,47 +1,91 @@
 import React from 'react';
 import axios from 'axios';
 import * as _ from 'lodash';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
+
+import './Search.css';
 
 class Search extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      query: ''
+      query: '',
+      data: [],
+      isVisible: false
     };
     this.handleValue = _.debounce(this.handleValue, 1000);
-    this.getAnalytics();
   }
 
-  handleValue (e) {
+  handleValue(e) {
     this.setState({
       [e.target.name]: e.target.value,
     });
     console.log(this.state.query);
+    this.getAnalytics();
+    this.setState({
+      isVisible: false
+    });
   }
 
   getAnalytics() {
-    axios.get('https://images-api.nasa.gov/search?q=mars')
+    axios.get(`https://images-api.nasa.gov/search?q=${this.state.query}`)
       .then((resp) => {
         console.log(resp);
+        this.setState({
+          data: resp.data.collection.items,
+          isVisible: true
+        })
+
       })
   }
 
   render() {
-    return(
+    let slider;
+
+    if (this.state.isVisible === true) {
+      slider = <Slider>
+        {this.state.data.map((item) => {
+            if (item.links === undefined) {
+              return (
+                <div className="Search__Elem">
+                  Not loaded image
+                </div>
+              )
+            } else {
+              return (
+                <div className="Search__Elem">
+                  <img src={item.links[0].href} alt="capture" key={item.data[0].nasa_id}/>
+                </div>
+              )
+            }
+          }
+        )}
+      </Slider>
+    } else {
+      slider = <div className="Search__NoResults"> No content, please type word in search</div>
+    }
+
+    return (
       <div className="Search">
-        <div>Im Analytics</div>
-        <input type="text"
-              name="query"
-              onChange={ e => {
-                e.persist();
-                this.handleValue(e)
-              }}
-        />
+        <div className="Search__Title">Search all Photos</div>
+        <div className="Search__Box">
+          <input type="text"
+                 name="query"
+                 className="Search__Input"
+                 onChange={e => {
+                   e.persist();
+                   this.handleValue(e)
+                 }}
+          />
+        </div>
+        <div className="Search__Body">
+          {slider}
+        </div>
       </div>
     )
   }
-
 }
 
-export { Search }
+export {Search}
