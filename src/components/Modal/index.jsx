5eb3a41c80d12415677bfddc4a98eeb00a8c76b3
Julian Blagis <julianbgs96@gmail.com>
2019-12-12 16:19:57 +0300

    add info

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -23,6 +23,12 @@ export default class Modal extends React.Component {
           </div>
           <div className="Modal__Body">
             <p className="Modal__Title">Open Nasa APIs</p>
+            <p className="Modal__Descr">The objective of this APIs is to make NASA data, including imagery,
+              eminently accessible to application developers.</p>
+            <div className="Modal__Info">
+              <p className="Modal__Author">Developer: Julian Blagis</p>
+              <a className="Modal__Link" href="https://github.com/Julianbgs">GitHub</a>
+            </div>
           </div>
         </div>
       </div>
